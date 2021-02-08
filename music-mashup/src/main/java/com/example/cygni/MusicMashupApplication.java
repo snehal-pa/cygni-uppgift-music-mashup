034daf3a570795af56d44c8d1495796f3f8c6483
snehal patel <snehal.kathiriya@gmail.com>
2021-02-08 08:50:24 +0100

    wip

---
+++ music-mashup/src/main/java/com/example/cygni/MusicMashupApplication.java
@@ -0,0 +1,13 @@
+package com.example.cygni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicMashupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicMashupApplication.class, args);
+	}
+
+}
---
+++ music-mashup/src/test/java/com/example/cygni/MusicMashupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cygni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicMashupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
